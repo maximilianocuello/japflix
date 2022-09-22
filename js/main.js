6d1b84ad4e36372c060ebd6fde16eb6e2f1cfbf6
Maximiliano Cuello <cuellomaximiliano0@gmail.com>
2022-09-22 12:21:16 -0300

    try showdata

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let pelis = undefined;
 let busqueda = undefined;
 let pelisFilter = undefined;
 
+
 /*CARGANDO DATA  */
 window.addEventListener('DOMContentLoaded', function loadScreen(){
   
@@ -12,20 +13,31 @@ window.addEventListener('DOMContentLoaded', function loadScreen(){
     }}
     )
   })
+  
+  function showData(arr){
+    debugger
+   let htmlContentToAppend = '';
+   for (let {title} of arr) {
+    
+     htmlContentToAppend += `<li>${title}</li>`;
+   }
+   document.getElementById('lista').innerHTML = htmlContentToAppend;
+   
+   
+  }
 
-
-  /*function filterSearch(search) {
-  })}*/
+  
   document.getElementById('btnBuscar').addEventListener('click', (e)=>{
     e.preventDefault;
     busqueda = document.getElementById('inputBuscar').value;
     
-    pelisFilter = pelis.filter(({title, overview, tagline})=>{
-      debugger
-      return title.toLowerCase().indexOf(busqueda.toLowerCase()) > -1;
+    
+    pelisFilter = pelis.filter(({title, genres , overview, tagline})=>{
+      let [{name}] = genres
+      return title.toLowerCase().indexOf(busqueda.toLowerCase()) > -1 || overview.toLowerCase().indexOf(busqueda.toLowerCase()) > -1 || tagline.toLowerCase().indexOf(busqueda.toLowerCase()) > -1 ||  name.toLowerCase().indexOf(busqueda.toLowerCase()) > -1;
      
       
-     /* filterSearch(busqueda)*/
-    })})
-/*  HACER OTRO FILTER PARA QUE APAREZCAN PELICULAS SIMILARES */
-/*  HACER UN SHOW PELIS */ 
+     
+    })
+    showData(pelisFilter);
+ })
