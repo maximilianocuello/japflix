70557bc6d6ff3cf135d4bf9482a822c5b08e9562
Maximiliano Cuello <110405289+maximilianocuello@users.noreply.github.com>
2022-09-23 13:10:32 -0300

    open container with info

--- js/main.js
+++ js/main.js
@@ -3,39 +3,42 @@ let busqueda = undefined;
 let pelisFilter = undefined;
 
 function showData(arr){
- let htmlContentToAppend = '';
+  
+ let htmlContentToShow = '';
  /* ME DA ERROR AL INICIAR LA FUNCION ONCLIC EN LA DIV  */
  for (let peli of arr) {
+  debugger
+  let {title, overview, genres, vote_average, tagline} = peli;
+  let generos = ''
+  for (let {name} of genres) {
+    console.log(name)
+    generos += `${name}   `;
+  }
+  htmlContentToShow += `<button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">${title}</button>
+   <div>
+   <h1 class="text-secondary">${tagline}</h1>
+   <h1 class="text-danger">${vote_average}</h1>
+   </div>
+   <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+   <div class="offcanvas-header">
+   <h5 class="offcanvas-title" id="offcanvasTopLabel">${title}</h5>
+   <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+   </div>
+   <div class="offcanvas-body">
+   <p>${overview}</p>
+   <p>${generos}</p>
+   <p></p>
+   </div>
+   </div>
+    
   
-   htmlContentToAppend += `<li>
-    <div onclick="showPeli(${peli})" cursor active>
-      <h1 class="text-white">${peli.title}</h1>
-      <h1 class="text-primary">${peli.tagline}</h1>
-      <h1 class="text-danger">${peli.vote_average}</h1>
-    </div>
-    </li>`;
+    `;
  }
- document.getElementById('lista').innerHTML = htmlContentToAppend;
+ document.getElementById('lista').innerHTML = htmlContentToShow;
  
  
 }
-function showPeli({title}){
-  let htmlContentToAppend = '';
-  htmlContentToAppend = `<button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">Toggle top offcanvas</button>
 
-  <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-    <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasTopLabel">Offcanvas top</h5>
-      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-    <div class="offcanvas-body">
-      <p>${title} </p>
-    </div>
-  </div>
-   `;
-   document.getElementById('lista').innerHTML = htmlContentToAppend;
-   
-}
 
 /*CARGANDO DATA  */
 window.addEventListener('DOMContentLoaded', function loadScreen(){
