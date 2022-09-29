67fa46d0d33e5488eb2c8627084051ba7a9eb5ad
Hector Alvarez <fakalvarez@gmail.com>
2022-09-29 11:05:30 -0300

    Boton more arreglado y con datos

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function showData(arr){
  /* ME DA ERROR AL INICIAR LA FUNCION ONCLIC EN LA DIV  */
  for (let peli of arr) {
   debugger
-  let {title, overview, genres, vote_average, tagline} = peli;
+  let {title, overview, genres, vote_average, tagline, release_date, runtime, budget, revenue} = peli;
   let generos = ''
   for (let {name} of genres) {
     console.log(name)
@@ -19,28 +19,31 @@ function showData(arr){
    <h1 class="text-secondary">${tagline}</h1>
    <h1 class="text-danger">${vote_average}</h1>
    </div>
-   <div class="offcanvas offcanvas-top" tabindex="1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+   <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
    <div class="offcanvas-header">
    <h5 class="offcanvas-title" id="offcanvasTopLabel">${title}</h5>
    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
    </div>
-   <div class="offcanvas-body" style="z-index:1000;overflow:hidden;">
+   <div style="overflow:hidden;margin-bottom;-5;margin-left:1%;">
    <p>${overview}</p>
-   <p>${generos}</p>
-   <p></p>
-   <div class="btn-group" role="group" style="z-index:1000;">
-   <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="true" style="position: right; z-index:1000; overflow:hidden;">
+   </div>
+    <hr>
+   <div style="display: flex; width: 100%;">
+   <p style="color:grey;margin-left:1%;">${generos}</p>
+   <div class="btn-group" role="group" style="width:10%;float:right;margin-left:75%; margin-bottom:1%;">
+   <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="true" style=" float:right; overflow:hidden;">
      More
    </button>
-   <ul class="dropdown-menu" style="z-index:1000; position:absolute;">
-     <li><a class="dropdown-item" href="#" style="z-index:1000; position:relative;">Year:</a></li>
-     <li><a class="dropdown-item" href="#" style="z-index:1000; position:relative;">Runtime:</a></li>
-     <li><a class="dropdown-item" href="#" style="z-index:1000; position:relative;">Budget:</a></li>
-     <li><a class="dropdown-item" href="#" style="z-index:1000; position:;">Revenue:</a></li>
+   <ul class="dropdown-menu">
+     <li><a class="dropdown-item" href="#" style="z-index:1000; position:static;">Year: ${release_date}</a></li>
+     <li><a class="dropdown-item" href="#" style="z-index:1000; position:static;">Runtime: ${runtime}</a></li>
+     <li><a class="dropdown-item" href="#" style="z-index:1000; position:static;">Budget: ${budget}</a></li>
+     <li><a class="dropdown-item" href="#" style="z-index:1000; position:static;">Revenue: ${revenue}</a></li>
    </ul>
  </div>
    
    </div>
+   
    </div>
     
   
