39bd772dad48811c205c145ae5dee473a636aade
jimenaagomez <emmagomezx@hotmail.com>
2022-10-02 15:12:55 -0300

    .
    .

--- js/main.js
+++ js/main.js
@@ -8,50 +8,56 @@ function showData(arr){
  /* ME DA ERROR AL INICIAR LA FUNCION ONCLIC EN LA DIV  */
  for (let peli of arr) {
   debugger
-  let {title, overview, genres, vote_average, tagline, budget, release_date,revenue, runtime} = peli;
+  let {title, overview, genres, vote_average, tagline, badget, release_date,revenue, runtime,} = peli;
   let generos = ''
   for (let {name} of genres) {
     
     generos += `${name}   `;
   }
-  htmlContentToShow += `<button class="btn btn-primary btn-item" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">${title}</button>
-   <div>
-   <h1 class="text-secondary">${tagline}</h1>
-   <h1 class="text-danger">${vote_average}</h1>
+  htmlContentToShow += `<button class="btn btn-primary btn-item" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop">${title}</button>
+   
+   <h1 class="text-secondary">${tagline}</h1><div>`
+   for(let i = 0; i<5; i++){
+    debugger
+    if (i<(vote_average/2)) {
+
+   htmlContentToShow +=`<span class="fa fa-star checked"></span>`;
+  }
+   else{
+    htmlContentToShow +=`<span class="fa fa-star" style="color: black"></span>`
+    }
+  }
+   htmlContentToShow += `
    </div>
-   <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+   <div class="offcanvas offcanvas-top" tabindex="1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
    <div class="offcanvas-header">
    <h5 class="offcanvas-title" id="offcanvasTopLabel">${title}</h5>
    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
    </div>
-   <div style="overflow:hidden;margin-bottom;-5;margin-left:1%;">
+   <div class="offcanvas-body" style="z-index:1000;overflow:hidden;">
    <p>${overview}</p>
-   </div>
-    <hr>
-   <div style="display: flex; width: 100%;">
-   <p style="color:grey;margin-left:1%;">${generos}</p>
-   <div class="btn-group" role="group" style="width:10%;float:right;margin-left:75%; margin-bottom:1%;">
-   <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="true" style=" float:right; overflow:hidden;">
+   <p>${generos}</p>
+   
+   <div class="btn-group" role="group" style="z-index:1000;">
+   <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="true" style="position: right; z-index:1000; overflow:hidden;">
      More
    </button>
-   <ul class="dropdown-menu">
-     <li><a class="dropdown-item" href="#" style="z-index:1000; position:static;">Year: ${release_date}</a></li>
-     <li><a class="dropdown-item" href="#" style="z-index:1000; position:static;">Runtime: ${runtime}</a></li>
-     <li><a class="dropdown-item" href="#" style="z-index:1000; position:static;">Budget: ${budget}</a></li>
-     <li><a class="dropdown-item" href="#" style="z-index:1000; position:static;">Revenue: ${revenue}</a></li>
+   <ul class="dropdown-menu" style="z-index:1000; position:absolute;">
+     <li><a class="dropdown-item" href="#" style="z-index:1000; position:relative;">Year:  ${release_date} </a></li>
+     <li><a class="dropdown-item" href="#" style="z-index:1000; position:relative;">Runtime:  ${runtime} </a></li>
+     <li><a class="dropdown-item" href="#" style="z-index:1000; position:relative;">Budget: ${badget}  </a></li>
+     <li><a class="dropdown-item" href="#" style="z-index:1000; position:;">Revenue:   ${revenue}  </a></li>
+   </ul>
  </div>
    
    </div>
-   
    </div>
-    
   
     `;
  }
  document.getElementById('lista').innerHTML = htmlContentToShow;
- 
- 
 }
+
 
 
 /*CARGANDO DATA  */
